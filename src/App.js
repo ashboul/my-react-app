45dc33eeb712cd1cedb1eeba834213ca3dc67cee
Abdullah Shboul <abdullah@Abdullahs-MacBook-Pro.local>
2020-11-29 19:22:40 +1100

    added a new feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Abdullah
         </a>
       </header>
     </div>
